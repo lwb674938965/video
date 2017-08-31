80589959d97d7961126a6a1eb60d3ae8081f9e38
Administrator <Administrator@2013-20160307YL>
2017-08-31 14:02:59 +0800

    1111111

--- src/main/java/com/zhiyou100/video/util/IdentifyingCode.java
+++ src/main/java/com/zhiyou100/video/util/IdentifyingCode.java
@@ -8,16 +8,16 @@ public class IdentifyingCode {
 		Random rand = new Random();
 		int code = 0;
 		if(length == 4){
-			code = rand.nextInt(9999-1000)+1;
+			code = rand.nextInt(9999-1000+1)+1000;
 			return code;
 		}else if(length == 5){
-			code = rand.nextInt(99999-10000)+1;
+			code = rand.nextInt(99999-10000+1)+10000;
 		}else if(length == 6){
-			code = rand.nextInt(999999-100000)+1;
+			code = rand.nextInt(999999-100000+1)+100000;
 		}else if(length == 7){
-			code = rand.nextInt(9999999-1000000)+1;
+			code = rand.nextInt(9999999-1000000+1)+1000000;
 		}else if(length == 8){
-			code = rand.nextInt(99999999-10000000)+1;
+			code = rand.nextInt(99999999-10000000+1)+10000000;
 		}		
 		return code;
 	}
